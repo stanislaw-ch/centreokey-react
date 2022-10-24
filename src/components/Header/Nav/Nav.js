8b703e539dd0fcc9b8074aea9265598a6843d19c
stanislaw-ch <post.chirko@gmail.com>
2022-10-24 21:49:07 +0200

    chore: WIP nav implementation

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -1,28 +1,56 @@
+import {useState} from "react";
 import classesRoot from '../../../App.module.css'
 import classes from './Nav.module.css'
 
 function Nav() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [activeIndex, setActiveIndex] = useState(1);
+
+    const navClasses = [
+        classes.mainNav,
+        isOpen ? classes.mainNavOpened: classes.mainNavClosed
+    ].join(" ");
+
+    const handleBurgerClick = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const handleMenuItemClick = (e) => {
+        const itemIndex = Number(e.target.getAttribute('data-key'))
+        setActiveIndex(itemIndex);
+    }
+
     return (
-        <nav className={`${classes.mainNav} ${classes.mainNavClosed}`}>
-            <button className={classes.mainNavToggle} type="button">
+        <nav className={navClasses}>
+            <button className={classes.mainNavToggle} type="button" onClick={handleBurgerClick}>
                 <span className={classesRoot.visuallyHidden}>Открыть меню</span>
             </button>
             <div className={classes.mainNavWrapper}>
                 <ul className={classes.siteList}>
-                    <li className={`${classes.siteListItem} ${classes.siteListItemOption1} ${classes.siteListItemActive}`}>
-                        <a href="src/components/Header/Header#">Главная</a>
+                    <li className={`${classes.siteListItem} ${classes.siteListItemOption1} ${activeIndex === 1? classes.siteListItemActive: null}`}
+                        onClick={handleMenuItemClick}
+                    >
+                        <a data-key={1}>Главная</a>
                     </li>
-                    <li className={`${classes.siteListItem} ${classes.siteListItemOption2}`}>
-                        <a href="src/components/Header/Header#">Комплексное развитие</a>
+                    <li className={`${classes.siteListItem} ${classes.siteListItemOption2} ${activeIndex === 2? classes.siteListItemActive: null}`}
+                        onClick={handleMenuItemClick}
+                    >
+                        <a data-key={2}>Комплексное развитие</a>
                     </li>
-                    <li className={`${classes.siteListItem} ${classes.siteListItemOption3}`}>
-                        <a href="src/components/Header/Header#">Английский для всех</a>
+                    <li className={`${classes.siteListItem} ${classes.siteListItemOption3} ${activeIndex === 3? classes.siteListItemActive: null}`}
+                        onClick={handleMenuItemClick}
+                    >
+                        <a data-key={3}>Английский для всех</a>
                     </li>
-                    <li className={`${classes.siteListItem} ${classes.siteListItemOption4}`}>
-                        <a href="src/components/Header/Header#">Арт-студия</a>
+                    <li className={`${classes.siteListItem} ${classes.siteListItemOption4} ${activeIndex === 4? classes.siteListItemActive: null}`}
+                        onClick={handleMenuItemClick}
+                    >
+                        <a data-key={4}>Арт-студия</a>
                     </li>
-                    <li className={`${classes.siteListItem} ${classes.siteListItemOption5}`}>
-                        <a href="src/components/Header/Header#">О нас</a>
+                    <li className={`${classes.siteListItem} ${classes.siteListItemOption5} ${activeIndex === 5? classes.siteListItemActive: null}`}
+                        onClick={handleMenuItemClick}
+                    >
+                        <a data-key={5}>О нас</a>
                     </li>
                 </ul>
             </div>
